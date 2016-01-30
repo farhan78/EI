248af3936a9f6d1943dff93c37fa1e6da95e3098
farhan78 <cold_fusion78@hotmail.com>
2016-01-30 17:29:59 +0000

    Service and Web project updates

---
+++ EI.Services/Abstract/IMembershipService.cs
@@ -0,0 +1,17 @@
+﻿using EI.Services.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EI.Services.Abstract
+{
+    public interface IMembershipService
+    {
+        MembershipContext ValidateUser(string username, string password);
+       // User CreateUser(string username, string email, string password, int[] roles);
+       // User GetUser(int userId);
+       // List<Role> GetUserRoles(string username);
+    }
+}
---
+++ EI.Web/App_Start/AutoFacWebapiConfig.cs
@@ -0,0 +1,68 @@
+﻿using Autofac;
+using Autofac.Core;
+using Autofac.Integration.WebApi;
+using EI.Data;
+using EI.Data.Infrastructure;
+using EI.Data.Repositories;
+using EI.Services;
+using EI.Web.Infrastructure.Core;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace EI.Web.App_Start
+{
+    public class AutofacWebapiConfig
+    {
+        public static Autofac.IContainer Container;
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder()));
+        }
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // EF EIContext
+            builder.RegisterType<EIContext>()
+                   .As<DbContext>()
+                   .InstancePerRequest();
+
+            builder.RegisterType<DbFactory>()
+                .As<IDbFactory>()
+                .InstancePerRequest();
+
+            builder.RegisterType<UnitOfWork>()
+                .As<IUnitOfWork>()
+                .InstancePerRequest();
+
+            builder.RegisterGeneric(typeof(EntityBaseRepository<>))
+                   .As(typeof(IEntityBaseRepository<>))
+                   .InstancePerRequest();
+
+            // Services
+            builder.RegisterType<EncryptionService>()
+                .As<IEncryptionService>()
+                .InstancePerRequest();
+
+            builder.RegisterType<MembershipService>()
+                .As<IMembershipService>()
+                .InstancePerRequest();
+
+            Container = builder.Build();
+
+            return Container;
+        }
+    }
+}
